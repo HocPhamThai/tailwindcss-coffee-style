2d0892d91eba5af945c6fd7ac2d8424c9727fe22
Pham Thai Hoc <89366106+HocerChicken@users.noreply.github.com>
2024-06-30 10:35:41 +0700

    Add feature mugs - product card

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,9 +19,14 @@ module.exports = {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0)' },
         },
+        fadeIn: {
+          from: { opacity: 0 },
+          to  : { opacity: 1 },
+        },
       },
       animation: {
-        slowDown: 'slowDown .4s ease-in-out',
+        slowDown: 'slowDown .5s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out',
       },
       backgroundImage: {
         'slider-bg': "url('./img/slider.jpg')",
